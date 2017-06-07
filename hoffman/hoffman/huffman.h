034511fa2f64cada6d7eb8a6e6249da6904b9c28
Shayla Nelson <xelamarie@gmail.com>
2017-06-06 19:13:55 -0700

    class update

--- hoffman/hoffman/huffman.h
+++ hoffman/hoffman/huffman.h
@@ -10,8 +10,25 @@
 
 #ifndef HUFFMAN_H
 #define HUFFMAN_H
-#include <string>
 
-void huffman(std::string fileName);
+#include "pair.h"
+#include "bnode.h"
+#include "list.h"
+#include <string>
+#include <iostream>
+
+class HuffmanCode
+{
+public:
+   HuffmanCode() { }
+   void huffman(std::string filename);
+   void load(std::string filename); // translate into a list of pairs, merge pairs into tree (private functions)
+   void display(std::ostream & out); // iterate over the list of pairs in correct order using the bNodes
+private:
+   List<Pair<std::string, double>> pairs;
+   List<BinaryNode> bNodes;
+   void getPairs(); // read from file
+   void buildTree(); // final bNodes tree
+};
 
 #endif // HUFFMAN_h
