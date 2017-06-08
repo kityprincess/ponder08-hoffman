502168a16779ba37c2ff686f658ee7d9155817f7
Shayla Nelson <xelamarie@gmail.com>
2017-06-07 17:14:41 -0700

    getPairs function

--- hoffman/hoffman/huffman.cpp
+++ hoffman/hoffman/huffman.cpp
@@ -9,14 +9,93 @@
  ************************************************************************/
 
 #include "huffman.h"       // for HUFFMAN() prototype
+#include "pair.h"
+
 #include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
 using namespace std;
+
+/*******************************************
+ * GET PAIRS
+ * read the paired data from a file and add
+ * to a list
+ *******************************************/
+void HuffmanCode :: getPairs(string filename)
+{
+   // declare stream variable
+   ifstream fin(filename);
+
+   // check for read error
+   if (fin.fail())
+   {
+      cout << "Unable to open file " << filename << endl;
+      fin.close();
+      return;
+   }
+
+   // read in data
+   string line;
+   while (getline(fin, line))
+   {
+      try
+      {
+         // load the line into a stringstream
+         stringstream ss;
+         ss.str(line);
+
+         // declare variables
+         string token;
+         double frequency;
+
+         // read in the data
+         ss >> token >> frequency;
+
+         // create a pair
+         Pair<string, double> pair = Pair<string, double>(token, frequency);
+         pairs.push_back(pair);
+      }
+
+      // catch any errors
+      catch (string message)
+      {
+         cout << message << endl;
+      }
+   }
+   fin.close();
+
+   return;
+}
+
+/*******************************************
+ * LOAD
+ * load the data, translate into a list of pairs,
+ * and merge pairs into tree
+ *******************************************/
+void HuffmanCode :: load(string filename)
+{
+   getPairs(filename);
+   return;
+}
+
+/*******************************************
+ * DISPLAY
+ * display the huffman codes
+ *******************************************/
+void HuffmanCode :: display(ostream & out)
+{
+   return;
+}
 
 /*******************************************
  * HUFFMAN
  * Driver program to exercise the huffman generation code
  *******************************************/
-void huffman(string fileName)
+void huffman(std::string filename)
 {
+   HuffmanCode hc;
+   hc.load(filename);
    return;
 }
--- hoffman/hoffman/huffman.h
+++ hoffman/hoffman/huffman.h
@@ -23,13 +23,12 @@ class HuffmanCode
 {
 public:
    HuffmanCode() { }
-   //void huffman(std::string filename);
    void load(std::string filename); // translate into a list of pairs, merge pairs into tree (private functions)
    void display(std::ostream & out); // iterate over the list of pairs in correct order using the bNodes
 private:
    List<Pair<std::string, double> > pairs;
    List<BinaryNode<double> * > bNodes;
-   void getPairs(); // read from file
+   void getPairs(std::string filename); // read from file
    void buildTree(); // final bNodes tree
 };
 
